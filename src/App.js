99dd36005456ed8ebc553157de1223d6255c8d8f
IsaacMagno <isaacmagno98@gmail.com>
2023-04-04 17:34:55 -0300

    att

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 
-function App() {
+import Homepage from "./pages/Homepage";
+
+const App = () => {
+  let data;
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (pathname == "/") return navigate("/pt");
+  }, []);
+
+  if (pathname.includes("pt")) {
+    data = require("./translate/PT_BR.json");
+  }
+
+  if (pathname.includes("fr")) {
+    data = require("./translate/FR.json");
+  }
+
+  if (pathname.includes("es")) {
+    data = require("./translate/ES.json");
+  }
+
+  if (pathname.includes("en")) {
+    data = require("./translate/EN.json");
+  }
+
+  useEffect(() => {
+    console.log("©2023 Isaac's Magno. All rights reserved.");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route
+        path='/pt'
+        element={<Homepage translateData={data} lang={pathname} />}
+      />
+      <Route
+        path='/fr'
+        element={<Homepage translateData={data} lang={pathname} />}
+      />
+      <Route
+        path='/en'
+        element={<Homepage translateData={data} lang={pathname} />}
+      />
+      <Route
+        path='/es'
+        element={<Homepage translateData={data} lang={pathname} />}
+      />
+    </Routes>
   );
-}
+};
 
 export default App;
